596773b20e2e4e0105e33caa249d2a9d686ce4bb
Jorge A <jorgea6973@gmail.com>
2021-04-18 23:08:23 -0500

    eslintrc y gitignore configurados

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../assets/styles/App.scss"
 
 const HelloWord = () => (
-    <h1>Hola Mundo WebServerDevonando</h1>
+    <h1>Hola Mundo</h1>
 );
 
 export default HelloWord;
