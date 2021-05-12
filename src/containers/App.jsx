64c032ceefbcb0ef860cd2343da7af037401e16e
Jorge A <jorgea6973@gmail.com>
2021-05-11 23:48:12 -0500

    Informacion Fake API Conectada

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,37 +7,55 @@ import CarouselItem from '../components/CarosuelItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState(
+    {
+      mylist: [], trends: [], originals: [] },
+  );
 
-    <Categories title='Mi Lista 2'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return (
+    <div className='app'>
+      <Header />
+      <Search />
 
-    <Categories title='Originales de Platzi'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      {
+        videos.mylist.length > 0 && (
+          <Categories title='Mi Lista'>
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+        )
+      }
 
-    <Footer />
-  </div>
-);
+      <Categories title='Tendencias'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
+
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de Platzi'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
