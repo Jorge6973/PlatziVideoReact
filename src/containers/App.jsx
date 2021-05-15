1930874191e535938bbeb06c21ac7bfa6b427feb
Jorge A <jorgea6973@gmail.com>
2021-05-14 22:08:56 -0500

    React Hooks InitialSatate.js Funcionando

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,27 +5,20 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarosuelItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initialState';
+
 const App = () => {
-  const [videos, setVideos] = useState(
-    {
-      mylist: [], trends: [], originals: [] },
-  );
-
-  useEffect(() => {
-    fetch('http://localhost:3000/initialState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
-
+  const initialState = useInitialState(API);
   return (
     <div className='app'>
       <Header />
       <Search />
 
       {
-        videos.mylist.length > 0 && (
+        initialState.mylist.length > 0 && (
           <Categories title='Mi Lista'>
             <Carousel>
               <CarouselItem />
@@ -38,7 +31,7 @@ const App = () => {
         <Carousel>
           {
             // eslint-disable-next-line react/jsx-props-no-spreading
-            videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+            initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)
           }
 
         </Carousel>
@@ -48,7 +41,7 @@ const App = () => {
         <Carousel>
           {
             // eslint-disable-next-line react/jsx-props-no-spreading
-            videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+            initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)
           }
         </Carousel>
       </Categories>
